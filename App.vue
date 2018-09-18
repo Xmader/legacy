fbb9aaa70bf6b7d2d6852cfe7f9fdb85b047142c
Xmader <xmader@outlook.com>
2018-09-18 15:22:39 +0800

    增加内容

--- App.vue
+++ App.vue
@@ -5,7 +5,8 @@
             <div class="card" v-for="(post, index) in postList" :key="index">
                 <a v-bind:href="post.link" target="_blank">
                     <img v-bind:src="post.img" />
-                    <small>Posted by: {{ post.author }}</small> {{ post.title }}
+                    <small>{{ post.description }}</small>
+                    {{ post.title }}
                 </a>
             </div>
         </div>
@@ -14,10 +15,10 @@
 
 <script>
 class Post {
-    constructor(title, link, author, img) {
+    constructor(title, link, description, img) {
         this.title = title;
         this.link = link;
-        this.author = author;
+        this.description = description || " "; // 引号里的不是空格，而是 U+2002 &ensp; En Space
         this.img = img;
     }
 }
@@ -28,10 +29,22 @@ export default {
             keyword: '',
             postList: [
                 new Post(
-                    'Vue.js',
-                    'https://vuejs.org/',
-                    'Chris',
-                    'https://vuejs.org//images/logo.png'
+                    '膜乎 App',
+                    'https://github.com/Xmader/mohu',
+                    '',
+                    'https://raw.githubusercontent.com/Xmader/mohu/master/resources/app/logo.png'
+                ),
+                new Post(
+                    '膜法指南 网页版',
+                    'https://github.com/Xmader/mogicians_manual',
+                    '无可奉告',
+                    'https://raw.githubusercontent.com/Xmader/mogicians_manual/master/icon.png'
+                ),
+                new Post(
+                    '乳包指南',
+                    'https://github.com/Xmader/rubao_manual',
+                    '',
+                    'https://raw.githubusercontent.com/Xmader/rubao_manual/master/icon.jpg'
                 ),
             ]
         }
