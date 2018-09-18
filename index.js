ef8e28a59b44641028b22bde5f7b72ebb561d06a
Xmader <xmader@outlook.com>
2018-09-18 14:20:28 +0800

    精简

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // const Vue = require("vue/dist/vue.js")
-import Vue from 'vue/dist/vue.esm'
+import Vue from 'vue/dist/vue.runtime.esm'
 import App from "./App.vue"
 
 var vm = new Vue({
     el: '#app',
-    components: {
-        "App": App
+    render: function (createElement) {
+        return createElement(
+            App,   // tag name 标签名称 或 组件对象
+        )
     }
 })
